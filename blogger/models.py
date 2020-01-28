64dd00c28b6013f308af7b4ac686c6862720d800
Ankit <ankitkpandey1@gmail.com>
2020-01-28 15:55:37 +0530

    adding models

--- blogger/models.py
+++ blogger/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    topicname=models.CharField(max_length=200, unique=True)
+    text=models.TextField()
+    pub_date=models.DateTimeField('Date published')
+
+class Comments(models.Model):
+    topic=models.ForeignKey(Topic, on_delete=models.CASCADE)
+    comtext=models.CharField(max_length=200)
+    com_date=models.DateTimeField('comment date')
+    
--- blogger/views.py
+++ blogger/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    txt="<h1> Welcome to home page </h1> <br><main>Content will be added soon!</main>"
+    return HttpResponse(txt)

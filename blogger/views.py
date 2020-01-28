8edd07c80753ee2a2d8d720b6bab744c02fd972a
Ankit <ankitkpandey1@gmail.com>
2020-01-28 18:16:30 +0530

    detail placeholder landing page done

--- blogger/views.py
+++ blogger/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Topic
+from .models import Topic, Comments
+from django.template import loader
 
 def home(request):
     latest=Topic.objects.order_by('-pub_date')[:5]
     output=",".join([q.topicname for q in latest])
+    template=loader.get_template('blogger/home.html')
+    
+    context={
+        'latest':latest,
+    }
     txt="<h1> Welcome to home page </h1> <br><main>Content will be added soon!</main>"
-    return HttpResponse(output)
+    return HttpResponse(template.render(context,request))
+
+
 def details(request, topic_id):
-    return HttpResponse("Details")
+   full=Topic.objects.get(pk=topic_id)
+   full.comments_set.all()
+   context={'full':full,}
+   return render(request,'blogger/details.html',context)

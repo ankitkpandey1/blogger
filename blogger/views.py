69d9ad39efab5a90ffa5393d0ba96e433105e8ea
Ankit <ankitkpandey1@gmail.com>
2020-01-28 20:03:30 +0530

    detail placeholder landing page done

--- blogger/views.py
+++ blogger/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Topic, Comments
 from django.template import loader
+from django.urls import reverse
+from django.utils import timezone
 
 def home(request):
     latest=Topic.objects.order_by('-pub_date')[:5]
@@ -18,5 +20,13 @@ def home(request):
 def details(request, topic_id):
    full=Topic.objects.get(pk=topic_id)
    full.comments_set.all()
-   context={'full':full,}
+   context={'full':full,}     
+   if request.method == "POST":
+       comt=request.POST.get("comtext",None)
+       q=full.comments_set.create(comtext=comt,com_date=timezone.now())
+       q.save()
+       full.comments_set.all()
+       context={'full':full,}   
+       render(request,'blogger/details.html',context)
    return render(request,'blogger/details.html',context)
+
